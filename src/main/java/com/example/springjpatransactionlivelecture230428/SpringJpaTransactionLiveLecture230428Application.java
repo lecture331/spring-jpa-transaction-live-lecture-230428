df329d6440b75190622bf0af39a28df16c0f23d7
Robbie <lecture@>
2023-04-28 18:54:36 +0900

    init commit

---
+++ src/main/java/com/example/springjpatransactionlivelecture230428/SpringJpaTransactionLiveLecture230428Application.java
@@ -0,0 +1,13 @@
+package com.example.springjpatransactionlivelecture230428;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaTransactionLiveLecture230428Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaTransactionLiveLecture230428Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springjpatransactionlivelecture230428/SpringJpaTransactionLiveLecture230428ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpatransactionlivelecture230428;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaTransactionLiveLecture230428ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
